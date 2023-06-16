9fa9f5eb885d433dfeefc7ca1f675ff2d4c5b86e
wanl1n <kate_young@dlsu.edu.ph>
2023-06-16 14:21:13 +0800

    documentation

--- soda/soda/soda.cpp
+++ soda/soda/soda.cpp
@@ -14,86 +14,90 @@
 #include <string>
 #include <iostream>
 
-float speed = 0.01f;
-float radius = 30.f;
-float scale_mod = 0.1f;
-float theta_tot = 90.f;
+float speed = 0.01f; // Movement speed of the camera.
+float radius = 30.f; // Distance of the spawned object to the camera.=
+float theta_tot = -90.f; // Max angle on the left or bottom side of the screen.
 
+// Initial Position of the Camera
 float x_mod = 0;
 float y_mod = 0;
 float z_mod = -50.f;
 
+// Movement flags. Updated by input.
 bool isMovingLeft = false;
 bool isMovingRight = false;
 bool isMovingForward = false;
 bool isMovingBack = false;
 
+// Spawn flag. Updated by input.
 bool isSpawning = false;
 
+// Create a 3D Model Object.
 class Model3D {
-private:
-    glm::vec3 pos;
-    glm::vec3 rotation; // axis of rotation
-    float theta;// in degrees
-    glm::vec3 scale;
-    std::vector<GLuint> mesh_indices;
 
-public:
-    Model3D(glm::vec3 pos, std::vector<GLuint> mesh_indices) {
-        this->pos = pos;
-        this->rotation = glm::vec3(0, 1, 0);
-        this->theta = 180.f;
-        this->scale = glm::vec3(scale_mod, scale_mod, scale_mod);
-        this->mesh_indices = mesh_indices;
-    }
+    // Worldly attributes of the 3D model (transformations and indices)
+    private:
+        glm::vec3 pos; // position of spawned model.
+        glm::vec3 rotation; // axis of rotation
+        float theta; // in degrees
+        glm::vec3 scale; // scale of the spawned model.
 
-    void draw(GLuint* shaderProgram, std::vector<GLfloat> fullVertexData) {
-        
-        /* * * * * * * * * * * * TRANSFORMING THE OBJECT * * * * * * * * * * * */
-        // Create the transformation matrix.
-        glm::mat4 transformation_matrix = glm::translate(glm::mat4(1.0f), this->pos);
-        transformation_matrix = glm::scale(transformation_matrix, this->scale);
-        transformation_matrix = glm::rotate(transformation_matrix,
-                                glm::radians(this->theta),
-                                this->rotation);
+    // Constructor and draw Function.
+    public:
+        // Initialize all attributes of an instance.
+        Model3D(glm::vec3 pos) {
+            this->pos = pos; // Take parameter as position.
 
-        unsigned int transformLoc = glGetUniformLocation(*shaderProgram, "transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformation_matrix));
+            // Since model is initially flipped (facing backwards), flip the model.
+            this->rotation = glm::vec3(0, 1, 0); // Set axis of rotation to y-axis.
+            this->theta = 180.f; // Rotate around y-axis by 180 degrees to flip the model.
 
-        glUseProgram(*shaderProgram);
+            // Since model is too big, scale it down to 0.1f.
+            this->scale = glm::vec3(0.1f, 0.1f, 0.1f);
+        }
 
-        //glDrawElements(GL_TRIANGLES, mesh_indices.size(), GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, fullVertexData.size() / 5);
-    }
+        void draw(GLuint* shaderProgram, std::vector<GLfloat> fullVertexData) {
+            // Create the transformation matrix and apply the transformation attributes at draw.
+            glm::mat4 transformation_matrix = glm::translate(glm::mat4(1.0f), this->pos);
+            transformation_matrix = glm::scale(transformation_matrix, this->scale);
+            transformation_matrix = glm::rotate(transformation_matrix,
+                                    glm::radians(this->theta),
+                                    this->rotation);
+
+            // Update the shader with the new transform of the object.
+            unsigned int transformLoc = glGetUniformLocation(*shaderProgram, "transform");
+            glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformation_matrix));
+
+            // Get the shaderprogram to be used.
+            glUseProgram(*shaderProgram);
+            // Draw the object (XYZUV in arrays) with texture.
+            glDrawArrays(GL_TRIANGLES, 0, fullVertexData.size() / 5);
+        }
 };
 
-void Key_Callback(
-    GLFWwindow* window,
-    int key,
-    int scancode,
-    int action,
-    int mod
-) {
+// Process keyboard inputs.
+void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mod) {
+    // If pressed D, set moving right to true.
     if (key == GLFW_KEY_D && action == GLFW_PRESS)
         isMovingRight = true;
+    // If released D, set moving right to false.
+    // The same applies for the A, W, and S.
     if (key == GLFW_KEY_D && action == GLFW_RELEASE)
         isMovingRight = false;
-
     if (key == GLFW_KEY_A && action == GLFW_PRESS)
         isMovingLeft = true;
     if (key == GLFW_KEY_A && action == GLFW_RELEASE)
         isMovingLeft = false;
-
     if (key == GLFW_KEY_W && action == GLFW_PRESS)
         isMovingForward = true;
     if (key == GLFW_KEY_W && action == GLFW_RELEASE)
         isMovingForward = false;
-
     if (key == GLFW_KEY_S && action == GLFW_PRESS)
         isMovingBack = true;
     if (key == GLFW_KEY_S && action == GLFW_RELEASE)
         isMovingBack = false;
 
+    // If pressed Spacebar, trigger a spawn.
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
         isSpawning = true;
 }
@@ -105,9 +109,11 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    // Height and width of the window.
     float height = 600.0f;
     float width = 600.0f;
 
+    // Creates window and ends program if it fails.
     window = glfwCreateWindow(height, width, "Kate Nicole Young", NULL, NULL);
     if (!window) {
         glfwTerminate();
@@ -117,8 +123,10 @@ int main(void)
     glfwMakeContextCurrent(window);
     gladLoadGL();
 
+    // Width, height, and color channels of a texture.
     int img_width, img_height, color_channels; // Width, Height, and color channels of the Texture.
 
+    // Flip the texture (because it's flipped by default).
     stbi_set_flip_vertically_on_load(true);
     // "Puss in banana suit 3D model" (https://skfb.ly/oF7Ay) by Wnight is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
     unsigned char* text_bytes = stbi_load("../3D/BananaCat_vertexcolor.png", // Texture path
@@ -126,17 +134,20 @@ int main(void)
                                           &img_height, // height of the texture
                                           &color_channels, // color channel
                                           0);
+    // Create a texture.
     GLuint texture;
     glGenTextures(1, &texture);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
+    // If the texture is too small, just clamp it to the edge.
     glTexParameteri(GL_TEXTURE_2D,
         GL_TEXTURE_WRAP_S,
         GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D,
         GL_TEXTURE_WRAP_T,
-        GL_REPEAT);
+        GL_CLAMP_TO_EDGE);
 
+    // Set the texture attributes using the width, height, and texture bytes taken from the loaded texture.
     glTexImage2D(
         GL_TEXTURE_2D,
         0, 
@@ -149,39 +160,52 @@ int main(void)
         text_bytes
     );
 
+    // Generate mipmaps/ smaller versions of the texture for faster rendering at further distances.
     glGenerateMipmap(GL_TEXTURE_2D);
+    // Free the memmory.
     stbi_image_free(text_bytes);
 
+    // Enable depth test to make sure the model has depth.
     glEnable(GL_DEPTH_TEST);
 
+    // Check for inputs of the user.
     glfwSetKeyCallback(window, Key_Callback);
 
+    // Shader program setup:
+    // Load the vertex shader from a source file.
     std::fstream vertSrc("../Shaders/sample.vert");
     std::stringstream vertBuff;
     vertBuff << vertSrc.rdbuf();
     std::string vertS = vertBuff.str();
     const char* v = vertS.c_str();
 
+    // Load the fragment shader from a source file.
     std::fstream fragSrc("../Shaders/sample.frag");
     std::stringstream fragBuff;
     fragBuff << fragSrc.rdbuf();
     std::string fragS = fragBuff.str();
     const char* f = fragS.c_str();
 
+    // Create a shader from the laoded vertex shader and compile it in the program.
     GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertShader, 1, &v, NULL);
     glCompileShader(vertShader);
 
+    // Create a shader from the loaded fragment shader and compile it in the program.
     GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragShader, 1, &f, NULL);
     glCompileShader(fragShader);
 
+    // Create a program where both fragment and vertex shaders will be attached.
+    // This will be used later when using the shaders.
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertShader);
     glAttachShader(shaderProgram, fragShader);
 
+    // Link the shader program to the program.
     glLinkProgram(shaderProgram);
 
+    // Load the 3D model obj file.
     // "Puss in banana suit 3D model" (https://skfb.ly/oF7Ay) by Wnight is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
     std::string path = "../3D/banancat.obj";
     std::vector<tinyobj::shape_t> shape;
@@ -189,6 +213,7 @@ int main(void)
     std::string warning, error;
     tinyobj::attrib_t attributes;
 
+    // Set its attributes from the obj file.
     bool success = tinyobj::LoadObj(
         &attributes,
         &shape,
@@ -198,63 +223,43 @@ int main(void)
         path.c_str()
     );
 
-    std::vector<GLuint> mesh_indices;
-    for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
-        mesh_indices.push_back(
-            shape[0].mesh.indices[i].vertex_index
-        );
-    }
-
-    // Loading the UV data of the object.
+    // Loading the Position and UV data of the object.
     std::vector<GLfloat> fullVertexData;
     for (int i = 0; i < shape[0].mesh.indices.size(); i++) {
         tinyobj::index_t vData = shape[0].mesh.indices[i];
-
-        // This will generate an array with 3 consecutive points 
-        // as a position coordinate ordered in the vector.
-        // X
         fullVertexData.push_back(attributes.vertices[vData.vertex_index * 3]);
-        // Y
         fullVertexData.push_back(attributes.vertices[vData.vertex_index * 3 + 1]);
-        // Z
         fullVertexData.push_back(attributes.vertices[vData.vertex_index * 3 + 2]);
-
-        // Add normals here
-        //fullVertexData.push_back(attributes.vertices[vData.normal_index * 3]);
-
-        // UV
         fullVertexData.push_back(attributes.texcoords[vData.texcoord_index * 2]);
         fullVertexData.push_back(attributes.texcoords[vData.texcoord_index * 2 + 1]);
         
-        // Result: 3 Pos, 2 UV, 3 Pos, 2 UV, and so on.
     }
 
+    // Generating buffers.
     GLuint VAO, VBO;
-        //EBO, VBO_UV;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    //glGenBuffers(1, &VBO_UV);
-    //glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Insert the model vertices (including UV) into the VBO for rendering).
     glBufferData(
         GL_ARRAY_BUFFER, 
         sizeof(GLfloat) * fullVertexData.size(),
         fullVertexData.data(),
         GL_STATIC_DRAW
     );
-
+    // Tell it how to interpret the aboce data.
     glVertexAttribPointer(
         0,
         3,
         GL_FLOAT,
         GL_FALSE,
-        //XYZ UV (change from 3 to 5)
         5 * sizeof(GL_FLOAT),
         (void*)0
     );
     
+    // Create a UV pointer (3 because its at index 3).
     GLintptr uvPtr = 3 * sizeof(float);
     glVertexAttribPointer(
         2,
@@ -267,34 +272,11 @@ int main(void)
     
     glEnableVertexAttribArray(0);
 
-    /*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(
-        GL_ELEMENT_ARRAY_BUFFER,
-        sizeof(GLuint) * mesh_indices.size(),
-        mesh_indices.data(),
-        GL_STATIC_DRAW
-    );*/
-
-    /*glBindBuffer(GL_ARRAY_BUFFER, VBO_UV);
-    glBufferData(GL_ARRAY_BUFFER,
-                 sizeof(GLfloat) * (sizeof(UV) / sizeof(UV[0])),
-                 &UV[0],
-                 GL_DYNAMIC_DRAW
-    );
-    glVertexAttribPointer(
-        2,
-        2,
-        GL_FLOAT,
-        GL_FALSE,
-        2 * sizeof(float),
-        (void*)0
-    );*/
-
     glEnableVertexAttribArray(2);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0); 
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+    // Set the projection view to perspective view.
     glm::mat4 projection = glm::perspective(
         glm::radians(60.f),
         height / width,
@@ -307,9 +289,10 @@ int main(void)
     // Stores all the models currently in the world.
     std::vector<Model3D*> models;
     // Spawn 1 model in at the start of the program.
-    models.push_back(new Model3D(glm::vec3(0, 0, 0), mesh_indices));
+    models.push_back(new Model3D(glm::vec3(0, 0, 0)));
     glfwSetTime(0); // Start the timer.
 
+    // Initialize the camera position.
     glm::vec3 cameraPos = glm::vec3(x_mod, y_mod, z_mod);
 
     while (!glfwWindowShouldClose(window))
@@ -319,29 +302,25 @@ int main(void)
         // Get the position of the cursor in the window.
         glfwGetCursorPos(window, &x_cursor_pos, &y_cursor_pos);
 
-        // Update the new x and y coordinates based on input.
-        /*if (isMovingLeft) x_mod -= speed;
-        if (isMovingRight) x_mod += speed;*/
-        //if (isMovingForward) z_mod -= speed;
-        //if (isMovingBack) z_mod += speed;
-
         /* * * * * ADJUSTING THE CAMERA ACCORDING TO THE MOUSE POSITION * * * * */
 
         // Calculate the position of the mouse with the origin (0, 0) at the center of the window.
         glm::vec2 mousePos = glm::vec2(x_cursor_pos - (width / 2), y_cursor_pos - (height / 2));
 
-        // Calculating the degree of rotation around the y-axis(yaw) and the x-axis (pitch).
         // Calculate the new camera center coordinates using Polar Coordinates.
+        // Calculating the degree of rotation around the y-axis(yaw) and the x-axis (pitch).
         // Assuming the screen is a number line from -90 to 90 (to represent the degree of rotation from the center/origin),
         // First, use linear interpolation to get the relative distance of the mouse cursor position.
         // Second, multiply it with the new "scale" (-90 to 90) to get the degree.
         // Third, convert it to radians.
         float yaw = glm::radians((mousePos.x / (width / 2)) * theta_tot);
-        float pitch = glm::radians((mousePos.y / (height / 2)) * -1 * theta_tot);
+        float pitch = glm::radians((mousePos.y / (height / 2)) * theta_tot);
 
         // Limiting the degree in case of flipping.
-        if (yaw > 90.f) yaw = 90.f;
-        if (pitch > 90.f) pitch = 90.f;
+        if (yaw > 89.9f) yaw = 89.9f;
+        if (yaw < -89.9f) yaw = -89.9f;
+        if (pitch > 89.9f) pitch = 89.9f;
+        if (pitch < -89.9f) pitch = -89.9f;
 
         // Finally get the direction in each axis by using Polar to Cartesian point conversion.
         float xAxisRot = radius * sin(yaw) * cos(pitch);
@@ -354,51 +333,17 @@ int main(void)
 
         // Next, calculate the position change based on where the camera center is.
         glm::vec3 worldUp = glm::normalize(glm::vec3(0, 1.f, 0));
-
-        if (isMovingLeft) cameraPos -= glm::normalize(glm::cross(cameraCenter, worldUp)) * speed;
-        if (isMovingRight) cameraPos += glm::normalize(glm::cross(cameraCenter, worldUp)) * speed;
+        
+        // If moving sideways, add or subtract the normalized right vector of the camera to move the camera position sideways.
+        if (isMovingLeft) cameraPos -= glm::normalize(glm::cross(cameraCenter, worldUp)) * 0.5f;
+        if (isMovingRight) cameraPos += glm::normalize(glm::cross(cameraCenter, worldUp)) * 0.5f;
+        // If moving forward or back, go towards or away from the camera center.
         if (isMovingForward) cameraPos += speed * (cameraCenter);
         if (isMovingBack) cameraPos -= speed * (cameraCenter);
 
-        //float xMoveDir = 0, yMoveDir = 0, zMoveDir = 0;        
-        //if (isMovingLeft || isMovingRight) {
-
-        //    if (isMovingLeft)
-        //    {
-        //        xMoveDir = speed * sin(glm::radians((mousePos.x / (width / 2)) * theta_tot - 90.f));
-        //        //yMoveDir = speed * sin(glm::radians((mousePos.y / (height / 2)) * -1 * theta_tot - 90.f));
-        //        zMoveDir = speed * cos(glm::radians((mousePos.x / (width / 2)) * theta_tot - 90.f)) * -1;
-        //    }
-        //    if (isMovingRight)
-        //    {
-        //        xMoveDir = speed * sin(glm::radians((mousePos.x / (width / 2)) * theta_tot + 90.f));
-        //        //yMoveDir = speed * sin(glm::radians((mousePos.y / (height / 2)) * theta_tot + 90.f));
-        //        zMoveDir = speed * cos(glm::radians((mousePos.x / (width / 2)) * theta_tot + 90.f));
-        //    }
-        //}
-
-        //if (isMovingForward || isMovingBack) {
-
-        //    xMoveDir += speed * sin(glm::radians((mousePos.x / (width / 2)) * theta_tot));
-
-        //    if (isMovingForward)
-        //    {
-        //        yMoveDir += speed * sin(glm::radians((mousePos.y / (height / 2)) * -1 * theta_tot));
-        //        zMoveDir += speed * cos(glm::radians((mousePos.x / (width / 2)) * theta_tot)) * -1;
-        //    }
-        //    if (isMovingBack)
-        //    {
-        //        yMoveDir += speed * sin(glm::radians((mousePos.y / (height / 2)) * theta_tot));
-        //        zMoveDir += speed * cos(glm::radians((mousePos.x / (width / 2)) * theta_tot));
-        //    }
-        //}
-        //
-        //glm::vec3 cameraPos = glm::vec3(x_mod += xMoveDir, y_mod += yMoveDir, z_mod += zMoveDir);
-
-        
         // Create the view matrix.
         glm::mat4 viewMatrix = glm::lookAt(cameraPos,
-                                            cameraCenter,
+                                           cameraPos+ cameraCenter, // to make sure cameracenter is always infront of camera pos.
                                             worldUp);
 
         // Updating the view Matrix in the shader program.
@@ -416,20 +361,20 @@ int main(void)
             if (glfwGetTime() > 3) {
                 glfwSetTime(0); // Reset the timer.
                 // Add a new model to be rendered at the point the camera is currently looking at.
-                models.push_back(new Model3D(cameraCenter, mesh_indices));
+                models.push_back(new Model3D(cameraPos + cameraCenter));
             }
             
             isSpawning = false; // Reset the flag.
         }
         
         /* * * * * * * * * * ADDING THE TEXTURE TO THE SHADERS * * * * * * * * * */
-        // Optional : Wanted to do this but might not be able to before due date. --! 
         GLuint tex0Address = glGetUniformLocation(shaderProgram, "tex0");
         glBindTexture(GL_TEXTURE_2D, texture);
         glUniform1i(tex0Address, 0);
 
         glBindVertexArray(VAO);
 
+        // Drawing all existing models.
         for (Model3D* model : models) {
             model->draw(&shaderProgram, fullVertexData);
         }
@@ -440,7 +385,6 @@ int main(void)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    //glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
